536b7629a766b52285330187c95bb28b6a5e7796
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-14 13:41:45 +0100

    Pagination added to products.views and products.html

--- products/views.py
+++ products/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Product
 
 # Create your views here.
 
 def all_products(request):
-    products = Product.objects.all()
+    product_list = Product.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(product_list, 12)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
     return render(request, "products.html", {"products": products})
