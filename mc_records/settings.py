c176009c4a8bb4f11d3c3f873b174ac12adacb8f
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-15 15:32:02 +0100

    Spelling error fixed in settings.py

--- mc_records/settings.py
+++ mc_records/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'mc_records.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if "DATABASE_URL" is os.environ:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
