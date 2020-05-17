00e8ed628918152ad441e0762bf2b0b1b8bc52ed
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-17 14:18:56 +0100

    added filtered products and some images to home.html

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from products.models import Product
 
 # Create your views here.
 
 def index(request):
     """A view that displays the home page"""
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    recently_released = Product.objects.order_by('-release_date').all()[:3]
+    return render(request, 'home.html',{"products": products, "recently_released": recently_released})
+
+
