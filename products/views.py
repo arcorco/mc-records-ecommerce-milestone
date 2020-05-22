afb05a91d905dfe783449354d7ebaf0a1f5f70df
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-22 15:47:29 +0100

    filtering now allows multiple artist choices and price range on nouislider

--- products/views.py
+++ products/views.py
@@ -5,19 +5,21 @@ from .filters import ProductFilter
 import random
 
 # Create your views here.
-
+def is_valid_queryparam(param):
+    return param != '' and param != [] and param is not None
 
 def all_products(request):
     product_list = Product.objects.all()
     artist_list = Product.objects.values_list('artist', flat=True).distinct()
-
-   
-    artist = request.GET.get('artist')
+    artist = request.GET.getlist('artist')
     genre = request.GET.get('genre')
     decade = request.GET.get('decade')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    print(artist)
 
     if is_valid_queryparam(artist):
-        product_list = product_list.filter(artist=artist)
+        product_list = product_list.filter(artist__in=artist)
 
     if is_valid_queryparam(genre):
         product_list = product_list.filter(genre=genre)
@@ -25,6 +27,12 @@ def all_products(request):
     if is_valid_queryparam(decade):
         product_list = product_list.filter(
             release_date__range=[decade+'-01-01', str(int(decade)+10)+'-01-01'])
+    
+    if is_valid_queryparam(price_min):
+        product_list = product_list.filter(price__gte=float(price_min))
+    
+    if is_valid_queryparam(price_max):
+        product_list = product_list.filter(price__lt=float(price_max))
     
     page = request.GET.get('page', 1)
     paginator = Paginator(product_list, 12)
@@ -37,16 +45,6 @@ def all_products(request):
 
     return render(request, "products.html", {"products": products, 'artist_list': artist_list,
          'genres': Product.GENRE_CHOICES, 'decades': [1970, 1980, 1990, 2000, 2010, 2020]})
-
-
-def is_valid_queryparam(param):
-    return param != '' and param is not None
-
-
-def product_filter(request):
-    return all_products(product_list)
-
-
 
 def product_detail(request, id):
     product_list = Product.objects.all()
---
+++ static/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $('.slider').slider();
+  $('select').formSelect();
+
+  var slider = document.getElementById('slider');
+  noUiSlider.create(slider, {
+    start: [0, 20],
+    step: 1,
+    connect: true,
+    range: {
+      'min': 0,
+      'max': 20
+    }
+  });
+
+  slider.noUiSlider.on('update', function (values, handle) {
+    var value = slider.noUiSlider.get();
+    price_min = value[0];
+    price_max = value[1];
+    console.log(value);
+    $('#price_min').attr("value", price_min);
+    $('#price_max').attr("value", price_max);
+  });
+
+  $('#filter-button').addEventListener.on('click', function() {
+    var artists = $("#artist").val();
+    $("#artist").attr("value", artists);
+    console.log(artists)
+  })
+
+});
