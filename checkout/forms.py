bd8d0c3b29784586be3892122915ee501417a99b
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-22 21:06:15 +0100

    Stripe API test added for payment and checkout functionality

--- checkout/forms.py
+++ checkout/forms.py
@@ -7,11 +7,11 @@ class MakePaymentForm(forms.Form):
 
     credit_card_number = forms.CharField(label="Credit card number", required=False)
     cvv = forms.CharField(label="Security code (CVV)", required=False)
-    expiry_month = forms.CharField(label="Month", choices=MONTH_CHOICES, required=False)
-    expiry_year = forms.CharField(label="Year", choices=YEAR_CHOICES, required=False)
+    expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(label="Year", choices=YEAR_CHOICES, required=False)
     stripe_id = forms.CharField(widget=forms.HiddenInput)
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = {'first_name', 'last_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'stree_address2', 'county'}
+        fields = {'first_name', 'last_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county'}
