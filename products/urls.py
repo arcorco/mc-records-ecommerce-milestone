f0afcb8412fbec3ef52f2dd892cc0aa6f2360a30
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-23 14:48:44 +0100

    decades.html added for users to view products from specific decades

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import all_products, product_detail
+from .views import all_products, product_detail, product_decades
 
 urlpatterns = [
     url(r'^$', all_products, name="products"),
+    url(r'^(?P<decade>\w+)$', product_decades, name="product_decades"),
     url(r'^(?P<id>\d+)$', product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -61,3 +61,44 @@ def product_detail(request, id):
     tracks = Track.objects.filter(album=product.id)
     random_albums = random.sample(list(product_list_without_current), k=3)
     return render(request, 'product_page.html', {'product': product, 'tracks': tracks, 'random_albums': random_albums})
+
+def product_decades(request, decade):
+    product_list = Product.objects.filter(release_date__range=[decade+'-01-01', str(int(decade)+10)+'-01-01'])
+    artist_list = Product.objects.values_list('artist', flat=True).distinct()
+    artist = request.GET.getlist('artist')
+    genre = request.GET.getlist('genre')
+    price_min = request.GET.get('price_min')
+    price_max = request.GET.get('price_max')
+    sort = request.GET.get('sort')
+
+    if is_valid_queryparam(sort):
+        if sort == "recent":
+            product_list = product_list.order_by('-id')
+        if sort == "lowhigh":
+            product_list = product_list.order_by('price')
+        if sort == "highlow":
+            product_list = product_list.order_by('-price')
+
+    if is_valid_queryparam(artist):
+        product_list = product_list.filter(artist__in=artist)
+
+    if is_valid_queryparam(genre):
+        product_list = product_list.filter(genre__in=genre)
+    
+    if is_valid_queryparam(price_min):
+        product_list = product_list.filter(price__gte=float(price_min))
+    
+    if is_valid_queryparam(price_max):
+        product_list = product_list.filter(price__lt=float(price_max))
+    
+    page = request.GET.get('page', 1)
+    paginator = Paginator(product_list, 12)
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, "decades.html", {"products": products, 'artist_list': artist_list,
+         'genres': Product.GENRE_CHOICES})
