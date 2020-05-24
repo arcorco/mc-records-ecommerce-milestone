1df5406dc688d9b0da9a724f08343c19e1a603ad
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-24 13:12:53 +0100

    checkout.models fields altered and migrated

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,7 +4,7 @@ from .models import Order
 class MakePaymentForm(forms.Form):
     MONTH_CHOICES = [(i, i) for i in range(1, 12)]
     YEAR_CHOICES = [(i, i) for i in range(2017, 2036)]
-
+    
     credit_card_number = forms.CharField(label="Credit card number", required=False)
     cvv = forms.CharField(label="Security code (CVV)", required=False)
     expiry_month = forms.ChoiceField(label="Month", choices=MONTH_CHOICES, required=False)
@@ -14,4 +14,4 @@ class MakePaymentForm(forms.Form):
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = {'first_name', 'last_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address1', 'street_address2', 'county'}
+        fields = {'first_name', 'last_name', 'phone_number', 'country', 'postcode', 'town_or_city', 'street_address', 'county'}
