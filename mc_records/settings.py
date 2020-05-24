d79eae658726c2f248094bf7fbcd488549cf49dc
Amelia Corcoran <milliecorcoran@virginmedia.com>
2020-05-24 18:52:14 +0100

    Additional styling to home.html, products.html and product-page.html

--- mc_records/settings.py
+++ mc_records/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
+#import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'django_forms_bootstrap',
     'products',
     'cloudinary',
     'cart',
